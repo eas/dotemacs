dadf77cb8e9bcc5c2e7388ea1fe9fab75e091099
Andrei Elovikov <a.elovikov@gmail.com>
2014-08-13 07:57:36 +0700

    Use "C-n/p" in company auto-completion list.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,6 +1,9 @@
 (use-package company
   :ensure company
   :config
-  (global-company-mode))
+  (progn
+    (bind-key "C-n" 'company-select-next company-active-map)
+    (bind-key "C-p" 'company-select-previous company-active-map)
+    (global-company-mode)))
 
 (provide 'init-company)
