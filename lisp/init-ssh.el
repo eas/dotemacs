20b797a243903ff6e426cd8523150fe41c59d499
Andrei Elovikov <a.elovikov@gmail.com>
2014-08-24 00:13:29 +0700

    Add some TODOs to `init-ssh.el'.

--- lisp/init-ssh.el
+++ lisp/init-ssh.el
@@ -1,6 +1,18 @@
+(require 'use-package)
+(require 'advice)
+
+(use-package dirtrack)
 (use-package ssh
   :ensure
+  :commands (ssh)
+  ;; TODO: http://enthusiasm.cozy.org/archives/2005/08/emacs-ssh-directory-tracking-via-tramp
   :init
+  (defadvice ssh (after ssh-fs-hook activate)
+    (message "tracking...")
+    (ssh-direcory-tracking-mode))
+  :config
   (progn
-    (setq-default ssh-directory-tracking-mode t)))
+    (setq-default ssh-directory-tracking-mode t)
+    ;; TODO: dirtrack-mode with regex depending on PS1
+    ))
 (provide 'init-ssh)
