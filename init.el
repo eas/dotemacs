226a51c57ea42c4cad56d4389382575ee4ad9542
Andrei Elovikov <andrei.elovikov@sifive.com>
2026-08-25 08:38:53 -0700

    Changes to `vertico-multiform` config

--- init.el
+++ init.el
@@ -169,15 +169,25 @@
 (use-package vertico
   :config
   (vertico-mode)
-  (vertico-multiform-mode))
+  (vertico-multiform-mode)
+  (setq vertico-multiform-commands '((consult-line
+                                      posframe
+                                      (vertico-posframe-poshandler . posframe-poshandler-frame-bottom-center)
+                                      (vertico-posframe-border-width . 10)
+                                      ;; NOTE: This is useful when emacs is used in both in X and
+                                      ;; terminal, for posframe do not work well in terminal, so
+                                      ;; vertico-buffer-mode will be used as fallback at the
+                                      ;; moment.
+                                      (vertico-posframe-fallback-mode . vertico-buffer-mode))))
+  (setq vertico-multiform-categories '((file posframe grid)
+                                       (t posframe))))
 
 (use-package vertico-posframe
   :init
   (setq vertico-posframe-parameters   '((left-fringe  . 12)    ;; Fringes
                                         (right-fringe . 12)
                                         (undecorated  . nil))) ;; Rounded frame
-  :config
-  (vertico-posframe-mode 1))
+  )
 
 (use-package savehist
   :init
